249c6b1886ac6cca8b15fd4350d1460460fa0378
JaxonC_06 <jaxoncostain@outlook.com>
2025-06-17 14:08:12 -0600

    Added homepage component, which contains TopNavigation and PhotoList.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
-import TopNavigation from './components/TopNavigationBar';
-import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
+import HomeRoute from './components/HomeRoute';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
-  // const photos = [...Array(3)];
-  // const photoItems = photos.map((photo, index) => (
-  //   <PhotoListItem
-  //     key={index}
-  //     photo={sampleDataForPhotoListItem}
-  //   />
-  // ));
 
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
