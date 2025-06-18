87e23c58532f79ad06ff8583cedac340232e687f
JaxonC_06 <jaxoncostain@outlook.com>
2025-06-18 13:53:40 -0600

    Simplified the Photo modal, destructured props

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,10 +5,11 @@ import PhotoList from '../components/PhotoList';
 const PhotoDetailsModal = (props) => {
   const { photo, onClose, favorites, toggleFavorite, onPhotoClick } = props;
   const { id, location, similar_photos, urls, user } = photo;
+  const { regular } = urls;
+  const { profile, name } = user;
+  const { city, country } = location;
 
-  const similarPhotos = Object.keys(similar_photos).map(photo => {
-    return similar_photos[photo];
-  })
+  const similarPhotos = Object.values(similar_photos);
 
   return (
     <div className="photo-details-modal">
@@ -16,12 +17,12 @@ const PhotoDetailsModal = (props) => {
         <img src={closeSymbol} alt="close symbol" />
       </button>
       <div className='photo-details-modal__top-bar'>
-          <img className='photo-details-modal__image' src={urls.regular}/>
+          <img className='photo-details-modal__image' src={regular}/>
         <div className='photo-details-modal__photographer-details'>
-          <img className='photo-details-modal__photographer-profile' src={user.profile}/>
+          <img className='photo-details-modal__photographer-profile' src={profile}/>
           <div className='photo-details-modal__photographer-info'>
-            <p>{user.name}</p>
-            <p className='photo-details-modal__photographer-location'>{location.city}, {location.country}</p>
+            <p>{name}</p>
+            <p className='photo-details-modal__photographer-location'>{city}, {country}</p>
           </div>
         </div>
         <h1 className='photo-details-modal__header'>Similar Photos</h1>
