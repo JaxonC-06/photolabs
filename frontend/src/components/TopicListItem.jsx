e0111b10b759199a98b7f88d5d56af225125f18c
JaxonC_06 <jaxoncostain@outlook.com>
2025-06-24 16:08:39 -0600

    Minor lint

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,7 +1,7 @@
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
-  const { id, slug, label, handleTopicClick } = props;
+  const { id, label, handleTopicClick } = props;
 
   return (
     <div className="topic-list__item" onClick={() => handleTopicClick(id)}>
